e5d06c399c2afd8ebf7108ea52a9403cc0bbd877
devi eswari <vanapallidevieswari@gmail.com>
2026-05-25 17:03:16 +0530

    Add files via upload

---
+++ condition statements.py
@@ -0,0 +1,47 @@
+#condition statements
+#if else statement
+# write a code to find out the greatest between two numbers and 
+# you to take input from the user and the inputs must and should be first _number and second_number
+
+'''First_number=int(input("Enter the first number"))
+second_number=int(input("Enter the second_number"))
+if(First_number>second_number):
+    print(f"First_number is greater than the second_number{First_number}")
+else:
+    print(f"second_number is greater than the First_number{Second_number} ")    
+o/p:Enter the first number40
+Enter the second_number35
+First_number is greater than the second_number40
+
+
+#2)Find out the number is even or not the number should be taken from the user
+ 
+ #Even
+number=int(input("Enter the number:"))
+if number%2==0:
+    print("Even")
+else:
+    print("not an even")
+    o/p:Enter the number:28
+Even
+
+#Odd
+number=int(input("Enter the number:"))
+if number%2==1:
+    print("odd")
+else:
+    print("not an odd")
+o/p:Enter the number:65
+odd
+Enter the number:22
+not an odd'''
+
+
+#Not equal to
+number=int(input("Enter the number:"))
+if number%2!=0:
+    print("odd")
+else:
+    print("not an odd")
+   ''' o/p:Enter the number:62
+not an odd'''
